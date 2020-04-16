4ec2e168fc2798d6bc974021e83e0508f7e52946
cheaterhats <58633075+cheaterhatsx@users.noreply.github.com>
2020-04-16 21:56:09 +0300

    Add files via upload

---
+++ hatshmv1.py
@@ -0,0 +1,42 @@
+giris = """
+7777    777777777777777777777777777777777777777777  
+7777    7777        777      77     77
+77777777777777777777777      77     7777777777
+7777    7777        777      77            77
+7777    7777        777      77  77777777777
+
+        Coded By CheaTeR HATS     *************
+Hats Hesap Makinesi    >> İnstagram @turksiberokulu <<
+"""
+print(giris)
+sayi1 = float(input('1. Sayıyı Giriniz >>'))
+sayi2 = float(input('2. Sayıyı Giriniz >>'))
+
+islem = input('İşlemi Giriniz (+,-,*,/) >>')
+
+if islem == '+':
+    sonuc = sayi1 + sayi2
+    print('=============================')
+    print(sonuc)
+    print('=============================')
+elif islem == '-':
+    sonuc = sayi1 - sayi2
+    print('=============================')
+    print(sonuc)
+    print('=============================')
+elif islem == '*':
+    sonuc = sayi1 * sayi2
+    print('=============================')
+    print(sonuc)
+    print('=============================')
+elif islem == '/':
+    sonuc = sayi1 / sayi2
+    print('=============================')
+    print(sonuc)
+    print('=============================')
+else:
+    print('Hatalı Giriş, Tekrar Deneyiniz..')
+
+
+
+
